1339dc86b7e3f6d1036ae81dab0ead4c5b6870fc
LuanGuilherme <luangandrade10@hotmail.com>
2022-04-28 13:28:10 -0300

    initial commit

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace series.Interfaces {
+	public interface IRepositorio<T> {
+
+		List<T> Lista();
+
+		T RetornaPorId (int id);
+
+		void Insere (T objeto);
+
+		void Exclui (int id);
+
+		void Atualiza (int id, T objeto);
+
+		int ProximoId ();
+
+	}
+}
